4f87469ed29a987dcdca78979e917051469ff1bd
Sajeeve <srikumar.sajeevan@izonedevelopers.com>
2022-06-26 21:04:01 +0530

    expense tracker
    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {SpeechProvider} from '@speechly/react-client';
+
+import {Provider} from './context/context';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <SpeechProvider appId="302dadf3-a21a-4a04-9939-921c2c2c94d3" language="en-US">
+        <Provider>
+            <App/>
+        </Provider>
+    </SpeechProvider>,
+document.getElementById('root'));
